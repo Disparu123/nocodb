ae950fac3cbc5512ed05af6d6b74a2e9f7b98d51
LORD-RIAN <lordjulian.s.rian@gmail.com>
2026-07-06 15:47:29 +0200

    Merge pull request #14167 from nocodb/nc-oracle-issues

--- packages/nc-gui/components/dlg/QuickImport.vue
+++ packages/nc-gui/components/dlg/QuickImport.vue
@@ -229,9 +229,7 @@ const importBtnText = computed(() => {
   return getBtnText(importLoading.value || preImportLoading.value)
 })
 
-const disableImportButton = computed(
-  () => !templateEditorRef.value?.isValid || templateEditorRef.value?.canImport === false || isError.value,
-)
+const disableImportButton = computed(() => !templateEditorRef.value?.isValid || isError.value)
 
 function buildPreviewParserConfig(encoding = 'utf-8') {
   return {
@@ -416,7 +414,7 @@ function populateUniqueTableName(tn: string, draftTn: string[] = []) {
       const s = t.table_name.split('___')
       let target = t.table_name
       if (s.length > 1) target = s[1]
-      return target === `${tn}` || t.table_name === `${tn}` || t.title === `${tn}`
+      return target === `${tn}` || t.table_name === `${tn}`
     })
   ) {
     tn = `${tn}_${c++}`
